a655623ea38712bdfae5b387963067f3cc723ddf
eddy <eddy@binshops.com>
2021-09-24 19:37:57 +0330

    fetch products from API

--- packages/api-client/src/index.server.ts
+++ packages/api-client/src/index.server.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import { getBootstrap } from './api/getBootstrap';
 import { apiClientFactory } from '@vue-storefront/core';
+import { getProduct } from './api/getProduct';
 
 const onCreate = (settings) => {
   const client = axios.create({
@@ -16,7 +17,8 @@ const onCreate = (settings) => {
 const { createApiClient } = apiClientFactory<any, any>({
   onCreate,
   api: {
-    getBootstrap
+    getBootstrap,
+    getProduct
   }
 });
 
